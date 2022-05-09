edc24b4efa64f843e20a6ba3b8f0733657f48f05
Liam Brannigan <l.brannigan@analyticsengines.com>
2022-05-09 12:31:09 +0100

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Polars-Examples-Public
 
 For the Pandas Comparison see this notebook by Matthew Powers for the 100 m row dataset: https://github.com/coiled/coiled-resources/blob/main/local/speed-up-dask-10x.ipynb
+
+To run the notebooks start the container with:
+```bash
+bash deploy.sh lab
+```
+and then start the notebook server using the alias defined in the Dockerfile by running
+```bash
+jlab
+```
+
+or by running 
+```bash
+jupyter lab --allow-root --ip 0.0.0.0 --no-browser
+```
+
+
+You should then be able to run the notebook in your browser at http://localhost:8888/
--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@
 #Target can be 'dev', 'test' or 'deploy' as per Dockerfile stages
 TARGET=$1
 
-DOCKER_BUILDKIT=1 docker build -t polars-examples .
+DOCKER_BUILDKIT=1 docker build -t polars-examples-public .
 
 if [ "${TARGET}" == "dev" ]; then
-docker run  -it --rm  -v $(pwd):/usr/src/app -v docker_bash_history:/root/.bash_history polars-examples:latest /bin/bash
+docker run  -it --rm  -v $(pwd):/usr/src/app polars-examples-public:latest /bin/bash
 fi
 
 if [ "${TARGET}" == "app" ]; then
-docker run -it --rm -p 8501:8501  -v $(pwd):/usr/src/app polars-examples:latest /bin/bash
+docker run -it --rm -p 8501:8501  -v $(pwd):/usr/src/app polars-examples-public:latest /bin/bash
 fi
 
 if [ "${TARGET}" == "lab" ]; then
-docker run -it --rm -p 8888:8888  -v $(pwd):/usr/src/app polars-examples:latest /bin/bash
+docker run -it --rm -p 8888:8888  -v $(pwd):/usr/src/app polars-examples-public:latest /bin/bash
 fi
